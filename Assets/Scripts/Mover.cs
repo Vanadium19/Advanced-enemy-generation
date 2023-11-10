cbe316db06b556a2ebe61fb7b08996481d6e0930
Vladislav <GimliVlad@list.ru>
2023-11-10 13:26:56 +0300

    Add Awake

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -10,48 +10,36 @@ public class Mover : MonoBehaviour
     private float _startPositionX;
     private float _targetPositionX;
     private Vector3 _targetPosition;
-    private Coroutine _moving;
 
-    private void Start()
+    private void Awake()
     {
         _startPositionX = transform.position.x;
         _targetPosition = new Vector3(_finalPositionX, 0, 0);
-        StartMove();
-    }    
+    }
+
+    private void Start()
+    {
+        StartCoroutine(Move());
+    }
 
     private void SetTargetPosition()
     {
         _targetPositionX = transform.position.x == _finalPositionX ? _startPositionX : _finalPositionX;
         _targetPosition = new Vector3(_targetPositionX, 0, 0);
-    }
-
-    private void RestartMove()
-    {
-        StopMove();
-        StartMove();
-    }
-
-    private void StartMove()
-    {
-        _moving = StartCoroutine(Move());
-    }
-
-    private void StopMove()
-    {
-        if (_moving != null)
-            StopCoroutine(_moving);
-    }
+    }  
 
     private IEnumerator Move()
     {
-        while (transform.position != _targetPosition)
+        while (true)
         {
-            transform.position = Vector3.MoveTowards(transform.position, _targetPosition, _speed * Time.deltaTime);
-            yield return null;
-        }
+            if (transform.position == _targetPosition)
+                SetTargetPosition();
 
-        transform.position = _targetPosition;
-        SetTargetPosition();
-        RestartMove();
+            while (transform.position != _targetPosition)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, _targetPosition, _speed * Time.deltaTime);
+                yield return null;
+            }
+        }               
     }
 }
